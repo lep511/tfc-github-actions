2e8b558f7805f278118ee3a849e119e700320c6b
lep511 <estebanpbuday@gmail.com>
2024-09-27 15:20:40 -0300

    update

--- terraform/microservice-1.tf
+++ terraform/microservice-1.tf
@@ -23,16 +23,3 @@ resource "aws_dynamodb_table" "basic-dynamodb-table" {
     type = "S"
   }
 }
-
-module "lambda_function" {
-  source = "terraform-aws-modules/lambda/aws"
-
-  function_name = "rust-aws-lambda"
-  description   = "Create an AWS Lambda in Rust with Terraform"
-  runtime       = "provided.al2"
-  architectures = ["arm64"]
-  handler       = "bootstrap"
-
-  create_package         = false
-  local_existing_package = "lambda-api/target/lambda/lambda-api/bootstrap.zip"
-}
