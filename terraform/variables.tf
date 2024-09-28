e4f0ef97e326b0c51296c176d827355e2f002485
lep511 <estebanpbuday@gmail.com>
2024-09-28 14:07:12 -0300

    update

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,19 +2,19 @@
 variable "application_name" {
   type        = string
   description = "The name of the application"
-  default     = "TerraformApp-dev"
+  default     = "TerraformApp-stage"
+}
+
+variable "environment" {
+  type        = string
+  description = "The name of the environment"
+  default     = "stage"
 }
 
 variable "region" {
   type        = string
   description = "The AWS region to deploy the application"
   default     = "us-east-1"
-}
-
-variable "environment" {
-  type        = string
-  description = "The name of the environment"
-  default     = "dev"
 }
 
 # DynamoDB Table Name
