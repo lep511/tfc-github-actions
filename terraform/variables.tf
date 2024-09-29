10380c0718aa38661a750b31de88f2858b4bd3b2
lep511 <estebanpbuday@gmail.com>
2024-09-29 10:41:25 -0300

    update

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,20 +1,20 @@
 # The region is specified in  GitHub - settings/variables/actions
-variable "region" {
-  type        = string
-  description = "The AWS region to deploy the application"
-  default     = "us-east-1"
-}
-
 variable "application_name" {
   type        = string
   description = "The name of the application"
-  default     = "TerraformApp-dev"
+  default     = "TerraformApp-prod"
 }
 
 variable "environment" {
   type        = string
   description = "The name of the environment"
-  default     = "dev"
+  default     = "prod"
+}
+
+variable "region" {
+  type        = string
+  description = "The AWS region to deploy the application"
+  default     = "us-east-1"
 }
 
 # DynamoDB Table Name
