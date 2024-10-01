95cfcbfbcf8256f5afe9906d809dc85a918a3a4e
lep511 <estebanpbuday@gmail.com>
2024-10-01 15:13:44 -0300

    update

--- lambda-api/src/main.rs
+++ lambda-api/src/main.rs
@@ -1,69 +1,20 @@
+use lambda_runtime::{service_fn, LambdaEvent, Error};
+use serde_json::{json, Value};
 use tracing_subscriber::filter::{EnvFilter, LevelFilter};
-use lambda_http::{run, service_fn, Body, Error, Request, RequestExt, Response};
+use aws_sdk_eventbridge as eventbridge;
 
-/// Handles an AWS Lambda HTTP request asynchronously.
-///
-/// # Arguments
-///
-/// * `event` - The incoming HTTP request event.
-///
-/// # Returns
-///
-/// Returns a `Result` containing the HTTP response or an error.
-///
-/// # Examples
-///
-/// ```rust
-/// use lambda_http::{Request, Body, Response};
-/// use lambda_http::http::Method;
-/// use std::convert::Infallible;
-///
-/// async fn handle_request(event: Request) -> Result<Response<Body>, Infallible> {
-///     // Handle the HTTP request here
-///     unimplemented!();
-/// }
-/// ```
-async fn handler(event: Request) -> Result<Response<Body>, Error> {
-    // Extract some useful information from the request
-    let who = event
-        .query_string_parameters_ref()
-        .and_then(|params| params.first("name"))
-        .unwrap_or("world");
-    let message = format!("Hello {who}, this is an AWS Lambda HTTP request");
-
-    // Return something that implements IntoResponse.
-    // It will be serialized to the right response event automatically by the runtime
-    let resp = Response::builder()
-        .status(200)
-        .header("content-type", "text/html")
-        .body(message.into())
-        .map_err(Box::new)?;
-    Ok(resp)
+async fn handler(event: LambdaEvent<Value>) -> Result<Value, Error> {
+    // Log the entire event
+    println!("Event: {:?}", event.payload);
+    
+    let payload = event.payload;
+    let first_name = payload["firstName"].as_str().unwrap_or("world");
+    
+    Ok(json!({ "message": format!("Hello, {first_name}!") }))
 }
 
-/// Initializes and runs the AWS Lambda HTTP handler.
-///
-/// # Returns
-///
-/// Returns a `Result` indicating success or failure.
-///
-/// # Errors
-///
-/// This function returns an error if there is an issue initializing the tracing subscriber or
-/// running the Lambda handler.
-///
-/// # Examples
-///
-/// ```rust
-/// use lambda_http::Error;
-///
-/// #[tokio::main]
-/// async fn main() -> Result<(), Error> {
-///     lambda_http_rust::run_lambda().await
-/// }
-/// ```
 #[tokio::main]
-async fn main() -> Result<(), Error> {
+async fn main() -> Result<(), eventbridge::Error> {
     tracing_subscriber::fmt()
         .with_env_filter(
             EnvFilter::builder()
@@ -75,6 +26,11 @@ async fn main() -> Result<(), Error> {
         // disabling time is handy because CloudWatch will add the ingestion time.
         .without_time()
         .init();
+    
+    let config = aws_config::load_from_env().await;
+    let client = aws_sdk_eventbridge::Client::new(&config);
 
+    lambda_runtime::run(service_fn(handler)).await
+    
     run(service_fn(handler)).await
 }
