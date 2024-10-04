75a08dfb32b47bbb1e2f143be1540a1f80dddb57
lep511 <estebanpbuday@gmail.com>
2024-10-04 17:42:16 -0300

    update first_deploy

--- lambda-api/src/main.rs
+++ lambda-api/src/main.rs
@@ -1,80 +1,74 @@
 use std::env;
-use tracing_subscriber::filter::{EnvFilter, LevelFilter};
-// use serde_json::{json, Value};
-use lambda_runtime::{run, service_fn, Error, LambdaEvent};
-use aws_sdk_dynamodb::types::AttributeValue;
-use aws_config::BehaviorVersion;
 use aws_lambda_events::{
-    event::sqs::{SqsBatchResponse, SqsEvent},
-    sqs::{BatchItemFailure, SqsMessage},
+    event::dynamodb::{StreamRecord},
+    event::sqs::{SqsBatchResponse},
+    sqs::{BatchItemFailure, SqsEventObj},
+    dynamodb::{EventRecord}
 };
+use aws_sdk_dynamodb::types::AttributeValue;
+use lambda_runtime::{run, service_fn, Error, LambdaEvent};
+use aws_config::BehaviorVersion;
+use serde::Deserialize;
+
 use aws_sdk_dynamodb::{
     Client, 
     Error as DynamoError
 };
 
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
 pub struct Item {
-    pub source_order: String,
-    pub source_item: String,
-    pub sku: String,
+    id: String,
+    name: String,
+    description: String,
+    custom_note: String
 }
 
 pub async fn add_item(client: &Client, item: Item, table: &String) -> Result<(), DynamoError> {
-    let s_order = AttributeValue::S(item.source_order);
-    let s_item = AttributeValue::S(item.source_item);
-    let s_sku = AttributeValue::S(item.sku);
+    let s_id = AttributeValue::S(item.id);
+    let s_name = AttributeValue::S(item.name);
+    let s_description = AttributeValue::S(item.description);
+    let s_custom_note = AttributeValue::S(item.custom_note);
 
     let request = client
         .put_item()
         .table_name(table)
-        .item("SourceOrderID", s_order)
-        .item("SourceItemID", s_item)
-        .item("Sku", s_sku);
-
-    // println!("Executing request [{request:?}] to add item...");
+        .item("id", s_id)
+        .item("name", s_name)
+        .item("description", s_description)
+        .item("note", s_custom_note);
 
     let _resp = request.send().await?;
 
     Ok(())
 }
 
-async fn process_record(message: &SqsMessage) -> Result<(), Error> {
-    println!("Process record: {:?}", message);
-    Err(Error::from("Error processing message"))
-}
 
-/// This is the main body for the function.
-/// Write your code inside it.
-/// There are some code example in the following URLs:
-/// - https://github.com/awslabs/aws-lambda-rust-runtime/tree/main/examples
-/// - https://github.com/aws-samples/serverless-rust-demo/
-async fn function_handler(event: LambdaEvent<SqsEvent>) -> Result<SqsBatchResponse, Error> {
-    let table_name =  env::var("DYNAMO_TABLE").expect("DYNAMO_TABLE must be set");
-    let mut batch_item_failures = Vec::new();
-
-    //let _detail_type = event.payload.detail_type;
-    //let _event_detail = event.payload.detail;
-    
+async fn process_record(stream: StreamRecord) -> Result<(), Error> {
+    let table  =  env::var("DYNAMO_TABLE").expect("DYNAMO_TABLE must be set");   
     let config = aws_config::load_defaults(BehaviorVersion::v2024_03_28()).await;
     let client = aws_sdk_dynamodb::Client::new(&config);
+    
+    let item: Item = serde_dynamo::from_item(stream.new_image.into_inner()).expect("(Error) Unwrapping Item");
+    tracing::info!("{:?}", item);
+    println!("Id = {}", item.id);
 
-    // tracing::info!("Received event: {:?}", event_detail);
+    let _ = add_item(&client, item, &table.to_string()).await;
 
-    let item = Item {
-        source_order: "testuser".into(),
-        source_item: "Brown".into(),
-        sku: "odata-27".into(),
-    };
+    Ok(())
 
-    let resp = add_item(&client, item, &table_name.to_string()).await;
+}
 
-    println!("Response: {:?}", resp);
+// function_handler
+// Lambda handler code for responding to events read from SQS
+async fn function_handler(event: LambdaEvent<SqsEventObj<EventRecord>>) -> Result<SqsBatchResponse, Error> {
+    let mut batch_item_failures = Vec::new();
 
-    for record in event.payload.records {
-        match process_record(&record).await {
+    for r in event.payload.records {
+        match process_record(r.body.change).await {
             Ok(_) => (),
             Err(_) => batch_item_failures.push(BatchItemFailure {
-                item_identifier: record.message_id.unwrap(),
+                item_identifier: r.message_id.unwrap(),
             }),
         }
     }
@@ -87,15 +81,14 @@ async fn function_handler(event: LambdaEvent<SqsEvent>) -> Result<SqsBatchRespon
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     tracing_subscriber::fmt()
-        .with_env_filter(
-            EnvFilter::builder()
-                .with_default_directive(LevelFilter::INFO.into())
-                .from_env_lossy(),
-        )
+        .json()
+        //.pretty()
+        .with_max_level(tracing::Level::INFO)
         // disable printing the name of the module in every log line.
         .with_target(false)
         // disabling time is handy because CloudWatch will add the ingestion time.
         .without_time()
+
         .init();
 
     run(service_fn(function_handler)).await
